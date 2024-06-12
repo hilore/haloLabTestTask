e72aa14cc33711864b303236b036a0741116ea0f
Mark Parker <marksadpepe@gmail.com>
2024-06-13 01:57:44 +0300

    minor changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import {createClient} from "redis";
-import express, {Request, Response} from "express";
+import express from "express";
 
 dotenv.config();
 
@@ -9,14 +9,10 @@ redisClient.on("error", (err) => {
   console.error("Redis Client Error:", err);
 });
 
-const port = process.env.PORT || 4000;
+const port: number = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({message: "Hello World 4"});
-});
 
 const startApp = async () => {
   await redisClient.connect();
