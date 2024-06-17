707346c086897083ec0bd69d126e16014b8b5842
Mark Parker <marksadpepe@gmail.com>
2024-06-17 12:21:45 +0300

    added a POST route to adjust sensor trusters speed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, {Request, Response} from "express";
 import WebSocket from "ws";
 import SensorManager from "./services/SensorManager";
+import requestMwd from "./middlewares/requestMiddleware";
 
 dotenv.config();
 
@@ -9,13 +10,39 @@ const port: number = Number(process.env.PORT) || 4000;
 const tickInterval: number = Number(process.env.TICK_INTERVAL) || 1000;
 
 const app = express();
-app.use(express.json());
-
 const server = app.listen(port, () => {
   console.log(`\nServer is listening on port ${port}`);
 });
 const wss = new WebSocket.Server({server});
 const sensorManager = new SensorManager();
+
+app.use(express.json());
+// TODO: move this logic somewhere else
+app.post(
+  "/sensor/:name/thruster",
+  requestMwd,
+  async (req: Request, res: Response) => {
+    try {
+      const name = req.params.name;
+      const {x, y, z} = req.body;
+      const sensorData = sensorManager.sensors.find((s) => s.name === name);
+      await sensorManager.adjustThrustersSpeed(name, x, y, z);
+
+      return res.status(200).json({
+        success: true,
+        sensor: {...sensorData}
+      });
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        const [statusCode, errMsg] = err.message.split(":");
+        return res.status(parseInt(statusCode)).json({
+          success: false,
+          message: errMsg
+        });
+      }
+    }
+  }
+);
 
 wss.on("connection", (ws: WebSocket.WebSocket) => {
   console.log("Client connected:");
